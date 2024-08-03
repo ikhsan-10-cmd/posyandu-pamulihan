aa298979d7669a39f891c51f80d8dcb802e16fea
afrijalindra20 <ym3965845@gmail.com>
2024-08-03 20:44:34 +0700

    perbarui file header dan index

--- includes/header.php
+++ includes/header.php
@@ -20,37 +20,37 @@ if (session_status() == PHP_SESSION_NONE) {
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="../views/index.php"> <!-- Update this link if necessary -->
-                <img src="../logo/logo.png" alt="Logo Posyandu" class="d-inline-block align-top"> <!-- Update this path if necessary -->
+            <a class="navbar-brand" href="index.php">
+                <img src="logo/logo.png" alt="Logo Posyandu" class="d-inline-block align-top">
                 Posyandu
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <li class="nav-item">
-            <a class="nav-link" href="../views/tasks.php"><i class="fas fa-tasks"></i> Tugas</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="../views/balita.php"><i class="fas fa-baby"></i> Data Balita</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="../crud/balita_crud.php"><i class="fas fa-user-edit"></i> Manajemen Balita</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="../crud/balita_detail.php"><i class="fas fa-ruler"></i> Pengukuran Balita</a>
-        </li>
-    <?php endif; ?>
-</ul>
+                <ul class="navbar-nav me-auto">
+                    <?php if (isset($_SESSION['user_id'])): ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="views/tasks.php"><i class="fas fa-tasks"></i> Tugas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="views/balita.php"><i class="fas fa-baby"></i> Data Balita</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="crud/balita_crud.php"><i class="fas fa-user-edit"></i> Manajemen Balita</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="crud/balita_detail.php"><i class="fas fa-ruler"></i> Pengukuran Balita</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
                 <?php if (isset($_SESSION['user_id'])): ?>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="../reset_password.php"><i class="fas fa-key"></i> Reset Password</a> <!-- Update this link if necessary -->
+                        <a class="nav-link" href="reset_password.php"><i class="fas fa-key"></i> Reset Password</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../login/logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a> <!-- Update this link if necessary -->
+                        <a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
                     </li>
                 </ul>
                 <?php endif; ?>
--- index.php
+++ index.php
@@ -314,4 +314,4 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 </script>
 
-<?php require_once __DIR__ . '/../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
